629a8a494fa62f30f078ab1f5fc354a728a20b47
FrancoRu <francoruggeri19@gmail.com>
2023-10-12 20:53:24 -0300

    Tp full resolve

--- Base de Datos/TPBD/TPBD.sql
+++ Base de Datos/TPBD/TPBD.sql
@@ -831,31 +831,25 @@ ORDER BY cantidad_jugadores DESC
 
 --11
 
-SELECT COUNT(EP.estadistica_partido_valor) FROM Partido P
-JOIN Equipo_Jugador EJ
-ON EJ.equipo_id = P.partido_local_equipo_id OR
-EJ.equipo_id = P.partido_visitante_equipo_id
-JOIN Equipo E
-ON E.equipo_id = EJ.equipo_id
-JOIN Jugador J
-ON J.jugador_id = EJ.jugador_id
-JOIN Division D
-ON D.division_id = E.equipo_division_id
-JOIN Equipo ER
-ON ER.equipo_id = P.partido_local_equipo_id OR
-ER.equipo_id = P.partido_visitante_equipo_id 
-JOIN Division DR
-ON DR.division_id = ER.equipo_division_id
-JOIN EstadisticaPartido EP
-ON EP.estadistica_partido_partido_id = P.partido_id AND EP.estadistica_partido_jugador_id = J.jugador_id
-JOIN Estadistica ES
-ON ES.estadistica_id = EP.estadistica_partido_estadistica_id
-WHERE (J.jugador_nombre = 'Kawhi' AND J.jugador_apellido = 'Leonard') AND 
-(D.division_conferencia_id <> DR.division_conferencia_id) AND
-(ES.estadistica_descripcion = 'Rebotes ofensivos' OR 
-ES.estadistica_descripcion = 'Rebotes defensivos' OR 
-ES.estadistica_descripcion = 'Asistencia' OR 
-ES.estadistica_descripcion = 'Puntos')
+SELECT
+    J.jugador_nombre AS Nombre,
+    J.jugador_apellido AS Apellido,
+    SUM(CASE WHEN ES.estadistica_descripcion IN ('Rebotes ofensivos', 'Rebotes defensivos') THEN EP.estadistica_partido_valor ELSE 0 END) AS Rebotes,
+    SUM(CASE WHEN ES.estadistica_descripcion = 'Asistencias' THEN EP.estadistica_partido_valor ELSE 0 END) AS Asistencias,
+    SUM(CASE WHEN ES.estadistica_descripcion = 'Puntos' THEN EP.estadistica_partido_valor ELSE 0 END) AS Puntos
+FROM Partido P
+JOIN Equipo_Jugador EJ ON EJ.equipo_id = P.partido_local_equipo_id OR EJ.equipo_id = P.partido_visitante_equipo_id
+JOIN Equipo E ON E.equipo_id = EJ.equipo_id
+JOIN Jugador J ON J.jugador_id = EJ.jugador_id
+JOIN Division D ON D.division_id = E.equipo_division_id
+JOIN Equipo ER ON ER.equipo_id = P.partido_local_equipo_id OR ER.equipo_id = P.partido_visitante_equipo_id
+JOIN Division DR ON DR.division_id = ER.equipo_division_id
+JOIN EstadisticaPartido EP ON EP.estadistica_partido_partido_id = P.partido_id AND EP.estadistica_partido_jugador_id = J.jugador_id
+JOIN Estadistica ES ON ES.estadistica_id = EP.estadistica_partido_estadistica_id
+WHERE (J.jugador_nombre = 'Kawhi' AND J.jugador_apellido = 'Leonard') 
+    AND (D.division_conferencia_id <> DR.division_conferencia_id)
+    AND (ES.estadistica_descripcion IN ('Rebotes ofensivos', 'Rebotes defensivos', 'Asistencias', 'Puntos'))
+GROUP BY J.jugador_nombre, J.jugador_apellido;
 
 --12
 
