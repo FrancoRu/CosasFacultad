b1eb52ec5a0aa64ae8383d40090720132755fe33
FrancoRu <francoruggeri19@gmail.com>
2023-11-05 11:54:15 -0300

    fix routes

---
+++ avanzada/List To do/server/views/layouts/footer.php
@@ -0,0 +1,11 @@
+ <footer class="footer">
+   <div class="footer-content">
+     <div class="footer-image">
+       <img src="/assets/footer.jpg" alt="Imágen para el pie de la FCyT">
+     </div>
+     <p>&copy; <?php echo date('Y'); ?> - BavBecFerRug</p>
+   </div>
+ </footer>
+ </body>
+
+ </html>
---
+++ avanzada/List To do/server/views/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Inicio de Sesion</title>
+  <link rel="stylesheet" href="/styles.css">
+</head>
+
+<body>
+  <div class="container">
+    <form>
+      <?php require_once 'views/partial/form.php' ?>
+      <input type="submit" value="Ingresar" class="app-button">
+    </form>
+    <p id="error-message" class="error" style="display: none;"></p>
+  </div>
+
+  <script src="login.js"></script>
+
+  <?php require_once 'views\\layouts\\footer.php'; ?>
